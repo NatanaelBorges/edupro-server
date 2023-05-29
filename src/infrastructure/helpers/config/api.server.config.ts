52d72e61f81753a98b9a8cfb0af81922c8e76ca6
Natanael Borges <natanael.dr.borges@gmail.com>
2023-05-29 11:44:38 +1000

    feat: create environments

---
+++ src/infrastructure/helpers/config/api.server.config.ts
@@ -0,0 +1,10 @@
+export class ApiServerConfig {
+  public static readonly HOST: string = process.env.HOST;
+
+  public static readonly PORT: number = parseInt(process.env.PORT);
+
+  // public static readonly ACCESS_TOKEN_HEADER: string = 'djkahdkjsahda';
+
+  public static readonly API_ACCESS_TOKEN_HEADER: string =
+    process.env.API_ACCESS_TOKEN_HEADER;
+}
